6f300636196e5afdac36d35163793ade4a6fab32
Xavier Perseguers <xavier@causal.ch>
2016-04-14 12:44:45 +0200

    [TASK] Kickstart extension

---
+++ ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "push_notification".
+ *
+ * Auto generated 04-04-2016 12:43
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Push Notification Service for iOS and Android',
+    'description' => 'Service to let extension developer send notifications to iOS and Android devices.',
+    'category' => 'services',
+    'shy' => 0,
+    'version' => '0.0.1',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Xavier Perseguers (Causal Sarl)',
+    'author_email' => 'xavier@causal.ch',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '6.2.0-8.99.99',
+            'realurl' => '',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+    '_md5_values_when_last_written' => '',
+);
