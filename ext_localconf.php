cb71735e387e5227ce91bb5fe96832ca3000c419
Xavier Perseguers <xavier@causal.ch>
2016-05-06 12:34:40 +0200

    [BUGFIX] Typo in scheduler task name

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,6 +7,6 @@ defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Causal\PushNotification\Service\AppleFeedbackTask::class] = [
     'extension' => $_EXTKEY,
-    'title' => 'Remove oudated iOS tokens',
+    'title' => 'Remove outdated iOS tokens',
     'description' => 'Connects to Apple Push Notification Feedback Service and retrieve outdated device tokens.',
 ];
