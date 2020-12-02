e69da3b9ae55517174f7acfc1f3e5328802cdc6c
Xavier Perseguers <xavier@causal.ch>
2020-12-02 08:57:55 +0100

    [TASK] Switch to static anonymous function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,14 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-/*****************************************************
- * Scheduler Tasks
- *****************************************************/
+(static function (string $_EXTKEY) {
+    /*****************************************************
+     * Scheduler Tasks
+     *****************************************************/
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Causal\PushNotification\Service\AppleFeedbackTask::class] = [
-    'extension' => $_EXTKEY,
-    'title' => 'Remove outdated iOS tokens',
-    'description' => 'Connects to Apple Push Notification Feedback Service and retrieve outdated device tokens.',
-];
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Causal\PushNotification\Service\AppleFeedbackTask::class] = [
+        'extension' => $_EXTKEY,
+        'title' => 'Remove outdated iOS tokens',
+        'description' => 'Connects to Apple Push Notification Feedback Service and retrieve outdated device tokens.',
+    ];
+})('push_notification');
