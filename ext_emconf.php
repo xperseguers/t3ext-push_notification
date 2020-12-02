50404409be0c38db16f56b988f6b1e6e2fa9d326
Xavier Perseguers <xavier@causal.ch>
2020-12-02 09:16:38 +0100

    [TASK] Drop deprecated options in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,21 +8,13 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Push Notification Service for iOS and Android',
     'description' => 'Service to let extension developer send notifications to iOS and Android devices.',
     'category' => 'services',
-    'shy' => 0,
     'version' => '1.1.0-dev',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
     'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
-    'modify_tables' => '',
     'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'Xavier Perseguers (Causal Sarl)',
     'author_email' => 'xavier@causal.ch',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
@@ -31,5 +23,4 @@ $EM_CONF[$_EXTKEY] = [
         'conflicts' => [],
         'suggests' => [],
     ],
-    '_md5_values_when_last_written' => '',
 ];
